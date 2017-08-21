0998da87e3078141c188a0e6c9e99e72e70bec01
Nick Cummings <nickmcummings@gmail.com>
2017-08-20 20:19:47 -0700

    Further updated devDependencies, including sass support

--- app.js
+++ app.js
@@ -2,5 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// CSS
+import './style.scss';
+
 console.log("Hello World");
 console.log("Testing changes");
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,12 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }, {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   }
